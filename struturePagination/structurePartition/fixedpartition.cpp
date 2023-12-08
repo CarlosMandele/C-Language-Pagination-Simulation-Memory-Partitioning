2a23236c645743d2bdc92407e0e49329ec79000a
CarlosMandele <carlos.mandelle@gmail.com>
2023-12-08 15:07:39 -0300

    Add update fixedpartition file

--- struturePagination/structurePartition/fixedpartition.cpp
+++ struturePagination/structurePartition/fixedpartition.cpp
@@ -3,8 +3,41 @@
     char processo[5];
 } particao;  particao RAM[10];
 
-int main() {
-  //inicialização da RAM
-  //alocação processo na RAM
-  //execução
+  int main() {
+  int i;
+  //inicialização da RAM, alocando processo na RAM e execução
+  for (i=0; i<10; i++)
+      RAM[i].bv=0;
+  
+  //alocação -> transformar em função
+  for (i=0; i<10; i++)
+     if (RAM[i].bv ==0) {
+         printf("Digite um processo: ");
+         RAM[i].bv = 1;
+         scanf("%",&RAM[i].processo);
+     }
+
+  //Simulação do processador encerrando a execução de processo
+  strcpy(RAM[2].processo,"");RAM[2].bv=0; printf("\n3o Processo finalizado\n");
+  strcpy(RAM[2].processo,"");RAM[8].bv=0; printf("\n9o Processo finalizado\n");
+
+  //Visualização da RAM simulada -> transforma em função
+  printf("\nStatus da RAM\n");
+  for (i=0; i<10; i++)
+        printf("%da Partição da RAM: [%d|%s]\n",i+1,RAM[i].bv,RAM[i].processo);
+
+  //Nova alocação
+  for (i=0; i<10; i++)
+     if (RAM[i].bv == 0) {
+        printf("\n");
+        printf("Digite um processo: ");
+        RAM[i].bv = 1;
+        scanf("%s",&RAM[i].processo);
+      }
+
+  // Nova visualização da RAM simulada
+  printf("\nStatus da RAM\n");
+  for (i=0; i<10; i++)
+        printf("%da Partição da RAM: [%d|%s]\n",i+1,RAM[i].bv,RAM[i].processo);
+  return(0);   
 }
