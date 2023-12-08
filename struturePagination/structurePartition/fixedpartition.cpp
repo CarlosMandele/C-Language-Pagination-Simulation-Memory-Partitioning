0c67dd767dae6eea7a8e904be34b37983b769c56
CarlosMandele <carlos.mandelle@gmail.com>
2023-12-08 00:56:23 -0300

    Create fixed partition file

---
+++ struturePagination/structurePartition/fixedpartition.cpp
@@ -0,0 +1,10 @@
+ typedef struct { 
+    unsigned int bv; // 0 = partição vazia
+    char processo[5];
+} particao;  particao RAM[10];
+
+int main() {
+  //inicialização da RAM
+  //alocação processo na RAM
+  //execução
+}
